d04f9add1e4207ea5c79b0d05a94ddcb72c6dacb
Madjid Taoualit <madjid.taoualit@etu.univ-lehavre.fr>
2022-01-20 00:19:21 +0100

    Adding initial project

---
+++ app/src/main/java/com/company/tracker/User.java
@@ -0,0 +1,24 @@
+package com.company.tracker;
+
+
+public class User {
+
+    private String email;
+    private String type;
+
+    public User() {}
+
+    public User(String email, String type) {
+        this.email = email;
+        this.type = type;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+}
